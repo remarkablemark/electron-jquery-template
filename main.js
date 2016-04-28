62159901e0c35d9574e47e133ed84e392caebc89
Mark <mark@remarkablemark.org>
2016-04-28 01:33:38 -0400

    Add main.js that creates a browser window for the app
    - Inspired by electron-quick-start:
      https://github.com/electron/electron-quick-start

---
+++ main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var electron = require('electron');
+var path = require('path');
+
+var mainWindow;
+
+/**
+ * Create the main browser window.
+ */
+function createWindow() {
+    mainWindow = new electron.BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+};
+
+/**
+ * App.
+ */
+electron.app.on('ready', createWindow);
+
+electron.app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+electron.app.on('activate', function() {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
